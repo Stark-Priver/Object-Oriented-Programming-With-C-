ea505c33117672af7ab0420305fee7142c5906f3
Priver <priverdeprince@yahoo.com>
2024-12-03 13:12:05 +0300

    Added the New Project C++ syntax

---
+++ C++_syntax/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age = 25;
+    float temperature = 26.5f;
+    char bloodGroup = 'O';
+    bool isParent = true;
+
+    cout <<"The age of respondent is :"<<age<<endl;
+    cout <<"the temperature of the Respondent is :"<<temperature<<endl;
+    cout <<"The respondent is in blood group: "<<bloodGroup<<endl;
+    cout <<"Is the respondent a Parent :"<<isParent<<endl;
+    return 0;
+}
