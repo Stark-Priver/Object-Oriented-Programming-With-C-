bf995f568c63457cd486d60349fbebcd9e2dedd2
Priver <priverdeprince@yahoo.com>
2024-12-03 13:32:14 +0300

    Added the New Project C++ Input_Output

---
+++ Input_output/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cout << "Enter a Number:" << endl;
+    cin >> number;//Taking the Input from the keyboard
+    cout <<"The Number you have Entered is :"<<number<<endl;
+}
